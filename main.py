03b90240ced9dd9fb44505aeb951d0feca16ea06
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-18 17:57:53 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
+numb1=int(input("enter first number:"))
+numb2=int(input("enter second number:"))
+print("the sum is:", add(numb1,numb2))
+print("the difference is:", subtract(numb1,numb2))
+print("the product is:", multiply(numb1,numb2))
+print("the quotient is:", divide(numb1,numb2))
